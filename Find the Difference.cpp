17b58a9e8cc0d6b2d9b10faf987759f1940a2e69
Nikhil Shrivastava <nikhilshrivastava175@gmail.com>
2022-02-07 13:18:10 +0530

    Day 7

---
+++ Find the Difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int count[26]={0};
+        for(int i=0;i<s.length();i++)
+            count[s[i]-'a']++;
+        for(int i=0;i<t.length();i++)
+            count[t[i]-'a']--;
+        
+        for(int i=0;i<26;i++)
+        {
+            if(count[i]==-1)
+                return (char)(i+'a');
+        }
+       return 'a'; 
+    }
+};
